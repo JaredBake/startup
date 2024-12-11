93b7f4332990be5f066f33ec521852f6215ea9fa
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2024-12-11 15:48:52 -0700

    Login and Create are not working

--- service/index.js
+++ service/index.js
@@ -1,3 +1,4 @@
+app.use(express.json());
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.static('public'));
 
--- src/login/login.js
+++ src/login/login.js
@@ -1,6 +1,25 @@
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
 
+// The scores and users are saved in memory and disappear whenever the service is restarted.
 let users = {};
-//CreateAuth a new user
+let scores = [];
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
   const user = users[req.body.email];
   if (user) {
@@ -24,4 +43,13 @@ apiRouter.post('/auth/login', async (req, res) => {
     }
   }
   res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', (req, res) => {
+  const user = Object.values(users).find((u) => u.token === req.body.token);
+  if (user) {
+    delete user.token;
+  }
+  res.status(204).end();
 });
