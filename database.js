d61f7ddc9d09c3ee98c183630473f612983232cb
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2024-12-11 22:22:55 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,2 +1,13 @@
 const config = require('./dbConfig.json');
 const url = `mongodb+srv://bakecs260:cs260password@bakecluster.ievjm.mongodb.net`;
+
+const client = new MongoClient(url);
+const db = client.db('rental');
+
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
